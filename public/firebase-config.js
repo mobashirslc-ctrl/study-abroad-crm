3688d7e76e6ec5077ef6c89934a98016ef623999
Mobashir <mobashir.slc@gmail.com>
2026-03-16 13:02:10 +0600

    Added firebase config and fixed 404 error

---
+++ public/firebase-config.js
@@ -0,0 +1,25 @@
+// public/firebase-config.js
+import { initializeApp } from "https://www.gstatic.com/firebasejs/10.7.1/firebase-app.js";
+import { getFirestore } from "https://www.gstatic.com/firebasejs/10.7.1/firebase-firestore.js";
+import { getAuth } from "https://www.gstatic.com/firebasejs/10.7.1/firebase-auth.js";
+import { getStorage } from "https://www.gstatic.com/firebasejs/10.7.1/firebase-storage.js";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyBxIzx-mzvUNdywOz5xxSPS9FQYynLHJlg",
+  authDomain: "scc-partner-portal.firebaseapp.com",
+  databaseURL: "https://scc-partner-portal-default-rtdb.firebaseio.com",
+  projectId: "scc-partner-portal",
+  storageBucket: "scc-partner-portal.firebasestorage.app",
+  messagingSenderId: "13013457431",
+  appId: "1:13013457431:web:9c2a470f569721b1cf9a52",
+  measurementId: "G-TX0NQE79MC"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+const db = getFirestore(app);
+const auth = getAuth(app);
+const storage = getStorage(app);
+
+// Export for other files
+export { db, auth, storage };
