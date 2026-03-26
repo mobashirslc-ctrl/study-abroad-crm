b36c94aad04ec75b51b88ad736f9d0b195778426
mobashirslc-ctrl <mobashir.slc@gmail.com>
2026-03-26 21:45:19 +0600

    Update withdrawal.js

--- api/withdrawal.js
+++ api/withdrawal.js
@@ -4,7 +4,7 @@ export default async function handler(req, res) {
     const client = new MongoClient(process.env.MONGODB_URI || process.env.MONGO_URI);
     try {
         await client.connect();
-        const database = client.db('crm_db');
+        const database = client.db('StudyAbroadCRM');
         const withdrawals = database.collection('withdrawals');
 
         if (req.method === 'POST') {
