0f7debdba08b0b105a60619a8f3434ce85e84e5b
mobashirslc-ctrl <mobashir.slc@gmail.com>
2026-03-28 03:22:49 +0600

    Update index.js

--- index.js
+++ index.js
@@ -234,7 +234,7 @@ app.post('/api/login', async (req, res) => {
     try {
         const { email, password } = req.body;
         const user = await User.findOne({ email: email.toLowerCase().trim() });
-        if (!user || !(await bcrypt.compare(password, user.password)){
+        if (!user || !(await bcrypt.compare(password, user.password))){
             return res.status(400).json({ msg: 'Invalid Credentials' });
         }
         res.json({ user: { 
